7fffbdfe7df41071d0450f09c46c92f5f8a4990f
its.chaitu03 <aradhyechaitanya@gmail.com>
2025-08-02 22:13:13 +0530

    Add files via upload

---
+++ Project09-StrateragyDPInjectionByUsingAliasXML/src/main/java/com/chaitu/Application.java
@@ -0,0 +1,27 @@
+package com.chaitu;
+
+import org.springframework.beans.BeansException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+import com.chaitu.sbeans.Journey;
+
+@SpringBootApplication
+@ImportResource("com/chaitu/cfgs/applicationContext.xml")
+public class Application {
+
+	public static void main(String[] args) {
+		try (ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args)) {
+			Journey bean = ctx.getBean("j", Journey.class);
+			System.out.println(
+					bean.traveling(new String[] { "Apple", "Cake", "Water bottle" }, new double[] { 100, 700, 20 }));
+		}
+
+		catch (BeansException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
